c14b4813973627bd5a8a4b8ee453a66085e17155
Simon Davis <s.davis@hotmail.co.nz>
2023-04-28 09:07:39 +0100

    calc code refactored

--- client/src/App.js
+++ client/src/App.js
@@ -15,101 +15,18 @@ const App = () => {
 
     console.log('WEIGHT', weight)
 
-    let doseReal = null
-
     if (weight <= 10) {
       const doseFinal = weight * 100
       console.log(`${doseFinal}ml per 24 hours.`)
       setDose(`${doseFinal}ml per 24 hours.`)
-      doseReal = doseFinal
-
     } else if (weight > 10 && weight <= 20) {
-      const diff = weight - 10
-      const subtenBracket = weight - diff
-
-      const doseSubTenBracket = subtenBracket * 100
-
-      const dosePlusTenBracket = diff * 50
-
-      const doseFinal = doseSubTenBracket + dosePlusTenBracket
-
-      console.log(`${doseFinal}ml per 24 hours.`)
-      setDose(`${doseFinal}ml per 24 hours.`)
-      return doseFinal
-
+      const dosePlusTen = ((weight - 10) * 50) + 1000
+      console.log(dosePlusTen)
+      setDose(`${dosePlusTen}ml per 24 hours.`)
     } else if (weight > 20) {
-
-      let dose10 = 0
-      let dose20 = 0
-      let plusTwentyWeight = 0
-
-      if (weight % 10 === 0) {
-        const num = weight / 10
-
-        for (let i = 1; i <= num; i++) {
-          if (i === 1) {
-            dose10 = 10 * 100
-          } else if (i === 2) {
-            dose20 = 10 * 50
-          } else if (i > 2) {
-            plusTwentyWeight++
-          }
-
-        }
-
-        plusTwentyWeight = plusTwentyWeight * 10
-
-        const plusTwentyDose = plusTwentyWeight * 20
-
-        console.log('DOSE FIRST 10kg', dose10)
-        console.log('DOSE SECOND 10kg', dose20)
-        console.log('remaining weight above 20kg', plusTwentyWeight)
-        console.log('DOSE PER KG OVER 20kg', plusTwentyDose)
-
-        const doseFinal = dose10 + dose20 + plusTwentyDose
-
-        console.log(`${doseFinal}ml per 24 hours.`)
-        setDose(`${doseFinal}ml per 24 hours.`)
-        doseReal = doseFinal
-        return
-
-      } else if (weight % 10 > 0) {
-        const remainder = weight % 10
-        console.log('Remainder', remainder)
-
-        const num = (weight / 10) - (remainder / 10)
-        console.log('num', num)
-
-        for (let i = 1; i <= num; i++) {
-          if (i === 1) {
-            dose10 = 10 * 100
-          } else if (i === 2) {
-            dose20 = 10 * 50
-          } else if (i >= 3) {
-            plusTwentyWeight++
-          }
-
-        }
-
-        plusTwentyWeight = plusTwentyWeight * 10 + remainder
-
-        const plusTwentyDose = plusTwentyWeight * 20
-
-        console.log('DOSE FIRST 10kg', dose10)
-        console.log('DOSE SECOND 10kg', dose20)
-        console.log('remaining weight above 20kg', plusTwentyWeight)
-        console.log('DOSE PER KG OVER 20kg', plusTwentyDose)
-
-        const doseFinal = dose10 + dose20 + plusTwentyDose
-
-        console.log(`${doseFinal}ml per 24 hours.`)
-        setDose(`${doseFinal}ml per 24 hours.`)
-        doseReal = doseFinal
-        return doseFinal
-      }
-
+      const dosePlusTwenty = ((weight - 20) * 20) + 1500
+      setDose(`${dosePlusTwenty}ml per 24 hours.`)
     }
-    console.log('DOSE REAL', doseReal)
 
   }
 
