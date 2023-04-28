3cbbb3d4eb28b6acc3f1bb957e218a11f74f5ae3
Simon Davis <s.davis@hotmail.co.nz>
2023-04-28 08:59:37 +0100

    React form working, basic calculator function all working. Needs styling. Logic for calculator can be cleaned up and shortened significantly

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
-import { useEffect } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+
 
 const App = () => {
 
-  const calc = (weight) => {
+  const [dose, setDose] = useState('')
+  const [weight, setWeight] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
     console.log('WEIGHT', weight)
+
+    let doseReal = null
+
     if (weight <= 10) {
-      const dose = weight * 100
-      console.log(`${dose}ml per 24 hours.`)
-      return (
-        `${dose}ml per 24 hours.`
-      )
+      const doseFinal = weight * 100
+      console.log(`${doseFinal}ml per 24 hours.`)
+      setDose(`${doseFinal}ml per 24 hours.`)
+      doseReal = doseFinal
+
     } else if (weight > 10 && weight <= 20) {
       const diff = weight - 10
       const subtenBracket = weight - diff
@@ -22,10 +34,8 @@ const App = () => {
       const doseFinal = doseSubTenBracket + dosePlusTenBracket
 
       console.log(`${doseFinal}ml per 24 hours.`)
-
-      return (
-        `${doseFinal}ml per 24 hours.`
-      )
+      setDose(`${doseFinal}ml per 24 hours.`)
+      return doseFinal
 
     } else if (weight > 20) {
 
@@ -50,16 +60,19 @@ const App = () => {
         plusTwentyWeight = plusTwentyWeight * 10
 
         const plusTwentyDose = plusTwentyWeight * 20
-      
+
         console.log('DOSE FIRST 10kg', dose10)
         console.log('DOSE SECOND 10kg', dose20)
         console.log('remaining weight above 20kg', plusTwentyWeight)
         console.log('DOSE PER KG OVER 20kg', plusTwentyDose)
-  
+
         const doseFinal = dose10 + dose20 + plusTwentyDose
-  
+
         console.log(`${doseFinal}ml per 24 hours.`)
-        
+        setDose(`${doseFinal}ml per 24 hours.`)
+        doseReal = doseFinal
+        return
+
       } else if (weight % 10 > 0) {
         const remainder = weight % 10
         console.log('Remainder', remainder)
@@ -74,9 +87,6 @@ const App = () => {
             dose20 = 10 * 50
           } else if (i >= 3) {
             plusTwentyWeight++
-            // const diff = weight - 20
-            // plusTwentyWeight = weight - diff
-            // break
           }
 
         }
@@ -84,30 +94,62 @@ const App = () => {
         plusTwentyWeight = plusTwentyWeight * 10 + remainder
 
         const plusTwentyDose = plusTwentyWeight * 20
-      
+
         console.log('DOSE FIRST 10kg', dose10)
         console.log('DOSE SECOND 10kg', dose20)
         console.log('remaining weight above 20kg', plusTwentyWeight)
         console.log('DOSE PER KG OVER 20kg', plusTwentyDose)
-  
+
         const doseFinal = dose10 + dose20 + plusTwentyDose
-  
+
         console.log(`${doseFinal}ml per 24 hours.`)
-
-
-
+        setDose(`${doseFinal}ml per 24 hours.`)
+        doseReal = doseFinal
+        return doseFinal
       }
 
-
-
-
-    } 
+    }
+    console.log('DOSE REAL', doseReal)
 
   }
 
-  calc(56)
+  const handleChange = (e) => {
+    setWeight(e.target.value)
+  }
 
-  return <h1>Hello World</h1>
+  const clearForm = (e) => {
+    e.preventDefault()
+    setDose('')
+    setWeight('')
+  }
+
+
+
+
+  return (
+    <main>
+
+      <div className='wrapper'>
+        <Form className='form' onSubmit={handleSubmit}>
+          <h1>IV Fluid Regime Calculator</h1>
+          <Form.Group className='mb-3'>
+            <Form.Label>Enter child&apos;s weight:</Form.Label>
+            <Form.Control placeholder='Input weight in kilograms' type='number' onChange={handleChange} value={weight}></Form.Control>
+          </Form.Group>
+          <h2>Result</h2>
+          <p>{dose ? `Dose is ${dose}` : 'Input weight into calculator.'}</p>
+          <Button variant='primary' type='submit'>Calculate</Button>
+          <Button variant='danger' type='reset' onClick={clearForm}>Clear</Button>
+        </Form>
+      </div>
+
+
+    </main>
+
+
+
+
+  )
 }
 
 export default App
